bee5ad5b3c03d4618ed4a827c138eb071b8b6e36
SimBuds <casey-hsu@outlook.com>
2023-10-17 07:06:18 -0400

    First Commit

---
+++ src/test/java/ca/georgebrown/gbc_springsocial68/GbcSpringSocial68ApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.georgebrown.gbc_springsocial68;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbcSpringSocial68ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
