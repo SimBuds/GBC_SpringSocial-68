c2647844a3a7268e8408e0889c1a129b69817c89
SimBuds <casey-hsu@outlook.com>
2023-10-17 12:55:06 -0400

    User Changes 1.0

---
+++ user-service/src/main/java/ca/georgebrown/userservice/model/User.java
@@ -0,0 +1,53 @@
+package ca.georgebrown.userservice.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @Size(max = 100)
+    private String fullName;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
