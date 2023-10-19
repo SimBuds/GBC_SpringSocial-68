5536b169f6acab5722d9fb9f05ca2df831bb43a4
SimBuds <casey-hsu@outlook.com>
2023-10-18 21:08:11 -0400

    User Fix 1.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,3 @@
 rootProject.name = "GBC_SpringSocial-68"
+
+include ("user-service")
--- user-service/src/main/java/ca/georgebrown/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/ca/georgebrown/userservice/service/UserServiceImpl.java
@@ -1,11 +1,15 @@
 package ca.georgebrown.userservice.service;
 
 import ca.georgebrown.userservice.dto.UserRequest;
+import ca.georgebrown.userservice.dto.UserResponse;
 import ca.georgebrown.userservice.model.User;
 import ca.georgebrown.userservice.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,25 +22,78 @@ public class UserServiceImpl implements UserService {
     public void createUser(UserRequest userRequest) {
         User user = new User();
         user.setUsername(userRequest.getUsername());
-        user.setPassword(userRequest.getPassword());
+        user.setPassword(userRequest.getPassword());  // Hashing needed
         user.setEmail(userRequest.getEmail());
         user.setFullName(userRequest.getFullName());
         userRepository.save(user);
     }
 
     @Override
-    public String updateUser(String userId, UserRequest userRequest) {
+    @Transactional
+    public String updateUser(String username, UserRequest userRequest) {
+        User user = userRepository.getUserByUsername(username);
+        if(user == null) {
+            throw new RuntimeException("User not found");
+        }
+        user.setUsername(userRequest.getUsername());
+        user.setPassword(userRequest.getPassword());  // Hashing needed
+        user.setEmail(userRequest.getEmail());
+        user.setFullName(userRequest.getFullName());
+        userRepository.save(user);
+        return user.getUsername();
+    }
+
+    @Override
+    public void deleteUser(String userId) {
         User user = userRepository.getUserByUsername(userId);
         if(user != null) {
-            user.setUsername(userRequest.getUsername());
-            user.setPassword(userRequest.getPassword());
-            user.setEmail(userRequest.getEmail());
-            user.setFullName(userRequest.getFullName());
-            return userRepository.save(user).getUsername();
+            userRepository.delete(user);
+        }
+    }
+
+    @Override
+    public List<UserResponse> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(this::convertToUserResponse).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUserId(String userId) {
+        User user = userRepository.getUserByUsername(userId);
+        if(user != null) {
+            return user.getUsername();
         }
         return userId;
     }
 
+    @Override
+    public String loginUser(UserRequest userRequest) {
+        User user = userRepository.getUserByUsername(userRequest.getUsername());
+        if(user != null) {
+            if(user.getPassword().equals(userRequest.getPassword())) {
+                return user.getUsername();
+            }
+        }
+        return null;
+    }
 
+    @Override
+    public String logoutUser(UserRequest userRequest) {
+        User user = userRepository.getUserByUsername(userRequest.getUsername());
+        if(user != null) {
+            if(user.getPassword().equals(userRequest.getPassword())) {
+                return user.getUsername();
+            }
+        }
+        return null;
+    }
 
+    private UserResponse convertToUserResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .fullName(user.getFullName())
+                .build();
+    }
 }
