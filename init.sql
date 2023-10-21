a8dcba6818aa61c85db474701c264bcd334d5c4b
SimBuds <casey-hsu@outlook.com>
2023-10-21 00:55:12 -0400

    docker-compose.yml adjustments

---
+++ init.sql
@@ -0,0 +1,44 @@
+-- Create the user
+CREATE USER myuser WITH PASSWORD 'mypass';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON DATABASE servicesdb TO myuser;
+
+-- Set the database to use
+\c servicesdb;
+
+-- Create the tables
+CREATE TABLE IF NOT EXISTS users (
+                                     id SERIAL PRIMARY KEY,
+                                     username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(120) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    fullName VARCHAR(100)
+    );
+
+CREATE TABLE IF NOT EXISTS comments (
+                                        id SERIAL PRIMARY KEY,
+                                        postId VARCHAR(255) NOT NULL,
+    content VARCHAR(250),
+    authorId VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP,
+    updatedAt TIMESTAMP
+    );
+
+-- Insert some data into users table
+INSERT INTO users (username, password, email, fullName)
+VALUES
+    ('user1', 'password1', 'user1@example.com', 'User One'),
+    ('user2', 'password2', 'user2@example.com', 'User Two');
+
+-- Insert some data into comments table
+INSERT INTO comments (postId, content, authorId, createdAt, updatedAt)
+VALUES
+    ('1', 'This is a comment', '1', '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
+    ('1', 'This is another comment', '2', '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
+    ('2', 'This is a comment', '1', '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
+    ('2', 'This is another comment', '2', '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
+    ('3', 'This is a comment', '1', '2019-01-01 00:00:00', '2019-01-01 00:00:00'),
+    ('3', 'This is another comment', '2', '2019-01-01 00:00:00', '2019-01-01 00:00:00');
+
+
--- mongo-init.js
+++ mongo-init.js
@@ -1,23 +1,30 @@
 // log the start of script execution
 print('START');
 
-// mongo-init.js
+// This will create a new database or switch to it if it already exists
+db = db.getSiblingDB('SpringSocialMongo');
 
+// Create a user with readWrite permissions on the SpringSocialMongo database
 db.createUser({
     user: "mongoadmin",
     pwd: "mypass",
     roles: [
         {
             role: "readWrite",
-            db: "testdb"
+            db: "SpringSocialMongo"
         }
     ]
 });
 
-// Use the database
-db = db.getSiblingDB('SpringSocialMongo');
+// Create the 'post' collection if it doesn't exist, as it will be used below
+db.createCollection('post');
 
-// Insert sample posts
+// Define a function to get the current date and time
+function NOW() {
+    return new Date();
+}
+
+// Insert sample posts into the 'post' collection
 db.post.insertMany([
     {
         "title": "Sample Post 1",
@@ -36,4 +43,4 @@ db.post.insertMany([
 ]);
 
 // log the end of script execution
-print('END')
+print('END');
