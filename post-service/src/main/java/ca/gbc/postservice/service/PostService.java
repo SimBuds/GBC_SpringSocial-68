8d04e5a66382ff4e7af6eda2ae92966e1f7fef8c
SimBuds <casey-hsu@outlook.com>
2023-12-09 12:49:17 -0500

    Discovery Service added

--- post-service/src/main/java/ca/gbc/postservice/service/PostService.java
+++ post-service/src/main/java/ca/gbc/postservice/service/PostService.java
@@ -2,13 +2,12 @@ package ca.gbc.postservice.service;
 
 import ca.gbc.postservice.dto.PostRequest;
 import ca.gbc.postservice.dto.PostResponse;
-
-import java.util.List;
+import reactor.core.publisher.Flux;
 
 public interface PostService {
     void createPost(PostRequest postRequest);
 
-    List<PostResponse> getAllPosts();
+    Flux<PostResponse> getAllPosts();
 
     String updatePost(String postId, PostRequest postRequest);
 
